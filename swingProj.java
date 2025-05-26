1aad18dd47d6749f83654aac5d002f99de61984f
sujalshukla102938 <Sujalnarayanshukla@gmail.com>
2025-05-26 20:40:39 +0530

    Add files via upload

---
+++ swingProj.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class swingProj {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setSize(700,800);
+        frame.setLayout(null);
+        frame.setVisible(true);
+        
+
+    }
+}
