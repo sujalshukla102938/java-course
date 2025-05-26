04afcc9a067f40e986f6fa6bb8d8209d3d5f1f0e
sujalshukla102938 <Sujalnarayanshukla@gmail.com>
2025-05-26 20:11:42 +0530

    Add files via upload

---
+++ Datatype.java
@@ -0,0 +1,13 @@
+public class Datatype {
+    public static void main(String args[]){
+        int a = 5;
+        double b = 6.99;
+        char c = 't';
+        boolean r = true; 
+        String t = "name the world";
+        System.out.println(a);
+        System.out.println(b);
+       System.out.println(c); 
+        System.out.println(r);
+    }
+}
